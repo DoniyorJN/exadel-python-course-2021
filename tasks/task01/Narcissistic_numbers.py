264e523678f1675cc682d574a30f6863a388144c
DoniyorJN <umidshoot@gmail.com>
2021-09-08 13:10:07 -0700

    changed logic of programm

--- tasks/task01/Narcissistic_numbers.py
+++ tasks/task01/Narcissistic_numbers.py
@@ -1,21 +1,18 @@
 def narNum(num):
-    if(num >= 1 and num <= 1000):
-        result = 0
-        division = 1
-        length = 0 
-        while(True):
-            division *= 10
-            length += 1
-            if(num//division==0):
-                break
-        for i in range(length):
-            division //= 10
-            result +=  (num//division%10) ** length
-        if(result == num):
-            print("This is Narcissistic number")
-        else:
-            print("This is NOT Narcissistic number")
-    else:
-        print("please enter number between 1 and 1000")
-
-narNum(1634)
+    result = 0
+    division = 1
+    length = 0 
+    while(True):
+        division *= 10
+        length += 1
+        if(num//division==0):
+            break
+    for i in range(length):
+        division //= 10
+        result +=  (num//division%10) ** length
+    if(result == num):
+        print(num)
+        
+   
+for i in range(0,1000):
+    narNum(i)
